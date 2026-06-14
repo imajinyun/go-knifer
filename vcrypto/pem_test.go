42c6842a55c0eed1cb7fdcb493860de5e1986acd
imajinyun <imajinyun@gmail.com>
2026-06-14 12:35:22 +0800

    test: split vcrypto rsa pem tests by responsibility

---
+++ vcrypto/pem_test.go
@@ -0,0 +1,96 @@
+package vcrypto_test
+
+import (
+	"bytes"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"errors"
+	"math/big"
+	"testing"
+	"time"
+
+	"github.com/imajinyun/go-knifer/vcrypto"
+)
+
+func TestPEMEncodeParseRSAKeys(t *testing.T) {
+	priv, err := vcrypto.GenerateRSAKey(1024)
+	if err != nil {
+		t.Fatal(err)
+	}
+	pubPEM, err := vcrypto.PublicKeyToPEM(&priv.PublicKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	pub, err := vcrypto.ParseRSAPublicKeyPEM(pubPEM)
+	if err != nil {
+		t.Fatal(err)
+	}
+	parsedPriv, err := vcrypto.ParseRSAPrivateKeyPEM(vcrypto.PrivateKeyToPEM(priv))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if parsedPriv.N.Cmp(priv.N) != 0 || pub.N.Cmp(priv.N) != 0 {
+		t.Fatal("parsed PEM keys do not match generated key")
+	}
+
+	pkcs8, err := vcrypto.PrivateKeyToPKCS8PEM(parsedPriv)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if _, err := vcrypto.ParseRSAPrivateKeyPEM(pkcs8); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := vcrypto.ParseRSAPublicKeyPEM(vcrypto.PublicKeyToPKCS1PEM(pub)); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestPEMParseInvalidKeys(t *testing.T) {
+	if _, err := vcrypto.ParseRSAPrivateKeyPEM([]byte("not pem")); !errors.Is(err, vcrypto.ErrInvalidKey) {
+		t.Fatalf("ParseRSAPrivateKeyPEM invalid = %v", err)
+	}
+	if _, err := vcrypto.ParseRSAPublicKeyPEM([]byte("not pem")); !errors.Is(err, vcrypto.ErrInvalidKey) {
+		t.Fatalf("ParseRSAPublicKeyPEM invalid = %v", err)
+	}
+	if _, err := vcrypto.ParseX509CertificatePEM([]byte("not pem")); !errors.Is(err, vcrypto.ErrInvalidKey) {
+		t.Fatalf("ParseX509CertificatePEM invalid = %v", err)
+	}
+}
+
+func TestPEMCertificateParseAndPublicKey(t *testing.T) {
+	priv, err := vcrypto.GenerateRSAKey(1024)
+	if err != nil {
+		t.Fatal(err)
+	}
+	pub := &priv.PublicKey
+	certDER, err := x509.CreateCertificate(bytes.NewReader(bytes.Repeat([]byte{0x42}, 1024)), &x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject:      pkix.Name{CommonName: "go-knifer.test"},
+		NotBefore:    time.Unix(0, 0),
+		NotAfter:     time.Unix(3600, 0),
+		KeyUsage:     x509.KeyUsageDigitalSignature,
+	}, &x509.Certificate{}, pub, priv)
+	if err != nil {
+		t.Fatalf("CreateCertificate: %v", err)
+	}
+	certPEM := pemEncode("CERTIFICATE", certDER)
+	cert, err := vcrypto.ParseX509CertificatePEM(certPEM)
+	if err != nil {
+		t.Fatalf("ParseX509CertificatePEM: %v", err)
+	}
+	if cert.Subject.CommonName != "go-knifer.test" {
+		t.Fatalf("certificate CN = %q", cert.Subject.CommonName)
+	}
+	certPub, err := vcrypto.PublicKeyFromCertificatePEM(certPEM)
+	if err != nil {
+		t.Fatalf("PublicKeyFromCertificatePEM: %v", err)
+	}
+	if certPub.N.Cmp(pub.N) != 0 {
+		t.Fatal("PublicKeyFromCertificatePEM returned different key")
+	}
+}
+
+func pemEncode(typ string, der []byte) []byte {
+	return pem.EncodeToMemory(&pem.Block{Type: typ, Bytes: der})
+}
---
+++ vcrypto/rsa_test.go
@@ -0,0 +1,103 @@
+package vcrypto_test
+
+import (
+	"bytes"
+	stdcrypto "crypto"
+	"crypto/rsa"
+	"crypto/sha256"
+	"testing"
+
+	"github.com/imajinyun/go-knifer/vcrypto"
+)
+
+func TestRSAEncryptDecryptAndSignVerify(t *testing.T) {
+	priv, err := vcrypto.GenerateRSAKey(1024)
+	if err != nil {
+		t.Fatal(err)
+	}
+	pub := &priv.PublicKey
+	plain := []byte("rsa message")
+
+	cipherText, err := vcrypto.RSAEncryptOAEP(plain, pub, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	out, err := vcrypto.RSADecryptOAEP(cipherText, priv, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(out, plain) {
+		t.Fatalf("RSADecryptOAEP() = %q", out)
+	}
+
+	digest := sha256.Sum256(plain)
+	pssSig, err := vcrypto.RSASignPSS(priv, stdcrypto.SHA256, digest[:])
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := vcrypto.RSAVerifyPSS(pub, stdcrypto.SHA256, digest[:], pssSig); err != nil {
+		t.Fatal(err)
+	}
+	quickSig, err := vcrypto.SignSHA256WithRSA(plain, priv)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := vcrypto.VerifySHA256WithRSA(plain, quickSig, pub); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRSAOptionsAndErrorPaths(t *testing.T) {
+	priv, err := vcrypto.GenerateRSAKey(1024)
+	if err != nil {
+		t.Fatal(err)
+	}
+	pub := &priv.PublicKey
+	plain := []byte("rsa message")
+	digest := sha256.Sum256(plain)
+
+	pssOptions := &rsa.PSSOptions{SaltLength: rsa.PSSSaltLengthEqualsHash, Hash: stdcrypto.SHA256}
+	pssSig, err := vcrypto.RSASignPSSWithOptions(priv, stdcrypto.SHA256, digest[:], vcrypto.WithRSAPSSOptions(pssOptions))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := vcrypto.RSAVerifyPSSWithOptions(pub, stdcrypto.SHA256, digest[:], pssSig, vcrypto.WithRSAPSSOptions(pssOptions)); err != nil {
+		t.Fatal(err)
+	}
+	if err := vcrypto.RSAVerifyPSS(pub, stdcrypto.SHA256, digest[:], append([]byte(nil), pssSig[:len(pssSig)-1]...)); err == nil {
+		t.Fatal("RSAVerifyPSS tampered signature error = nil")
+	}
+
+	oaepCipherText, err := vcrypto.RSAEncryptOAEPWithOptions(plain, pub, []byte("label"), vcrypto.WithRSAOAEPHash(sha256.New))
+	if err != nil {
+		t.Fatal(err)
+	}
+	oaepOut, err := vcrypto.RSADecryptOAEPWithOptions(oaepCipherText, priv, []byte("label"), vcrypto.WithRSAOAEPHash(sha256.New))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(oaepOut, plain) {
+		t.Fatalf("RSADecryptOAEPWithOptions() = %q", oaepOut)
+	}
+
+	digestSig, err := vcrypto.SignWithRSAOptions(
+		[]byte("rsa digest payload"),
+		priv,
+		vcrypto.WithRSADigestHash(stdcrypto.SHA256, sha256.New),
+		vcrypto.WithRSADigestRandomReader(bytes.NewReader(bytes.Repeat([]byte{0x66}, 512))),
+	)
+	if err != nil {
+		t.Fatalf("SignWithRSAOptions: %v", err)
+	}
+	if err := vcrypto.VerifyWithRSAOptions(
+		[]byte("rsa digest payload"),
+		digestSig,
+		pub,
+		vcrypto.WithRSADigestHash(stdcrypto.SHA256, sha256.New),
+	); err != nil {
+		t.Fatalf("VerifyWithRSAOptions: %v", err)
+	}
+	if err := vcrypto.VerifyWithRSAOptions([]byte("different"), digestSig, pub); err == nil {
+		t.Fatal("VerifyWithRSAOptions tampered data error = nil")
+	}
+}
